43378ee6dfe437fc2ce609c1fb48373814680cf6
Joshua Sanders <joshua_sandersr@yahoo.com>
2019-02-19 09:49:32 -0800

    remove some example code

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,23 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <!-- the router outlet, where all matched components would ber viewed -->
+    <router-view></router-view>
   </div>
 </template>
 
-<style lang="scss">
+<script>
+export default {
+  name: 'app'
+};
+</script>
+<!-- styling for the component -->
+<style>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: Matrix, 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  margin-top: 60px;
 }
 </style>
