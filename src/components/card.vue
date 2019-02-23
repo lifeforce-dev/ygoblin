b2530fea018b9c0aab3f8494c27adf9eba4023b0
Joshua Sanders <joshua_sandersr@yahoo.com>
2019-02-23 01:36:27 -0800

    Card component shows a card image

--- src/components/card.vue
+++ src/components/card.vue
@@ -1,11 +1,28 @@
 <template>
   <div class="main">
-    <h1>YGoblin</h1>
+    <v-content>
+      <v-btn color="info" id="get-data" @click="getData">Get Data</v-btn>
+    </v-content>
+    <h1 id="header">YGoblin</h1>
+    <v-img id="card-img" :src="imgUrl"></v-img>
   </div>
 </template>
 
 <script>
-export default { name: 'card' };
+export default {
+  name: 'card',
+  data() {
+    return {
+      imgUrl: ''
+    };
+  },
+  methods: {
+    getData() {
+      this.imgUrl =
+        'https://storage.googleapis.com/ygoprodeck.com/pics/61665245.jpg';
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
