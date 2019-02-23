8b8bd1bc0dd4d66fdfb2397118bcd76322e02736
Joshua Sanders <joshua_sandersr@yahoo.com>
2019-02-22 22:31:45 -0800

    remove some debug things

--- src/main.js
+++ src/main.js
@@ -4,10 +4,8 @@ import Vue from 'vue';
 import App from './App';
 import router from './router';
 
-console.log(router);
 Vue.config.productionTip = false;
 
-/* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
