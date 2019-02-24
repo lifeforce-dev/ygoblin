4ff99fbc56a6b16fbc24dc3addf4225fdebddcc4
Joshua Sanders <joshua_sandersr@yahoo.com>
2019-02-24 15:42:38 -0800

    reformat card.vue

--- src/components/card.vue
+++ src/components/card.vue
@@ -1,30 +1,29 @@
 <template>
-  <div class="main">
-    <v-content>
-      <v-btn color="info" id="get-data" @click="getData">Get Data</v-btn>
-    </v-content>
-    <h1 id="header">YGoblin</h1>
-    <v-img id="card-img" :src="imgUrl"></v-img>
-  </div>
+    <div class="main">
+        <v-content>
+            <v-btn color="info" id="get-data" @click="getData">Get Data</v-btn>
+        </v-content>
+        <h1 id="header">YGoblin</h1>
+        <v-img id="card-img" :src="imgUrl"></v-img>
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'card',
-  data() {
-    return {
-      imgUrl: ''
-    };
-  },
-  methods: {
-    getData() {
-      this.imgUrl =
-        'https://storage.googleapis.com/ygoprodeck.com/pics/61665245.jpg';
+    name: 'card',
+    data() {
+        return {
+            imgUrl: ''
+        };
+    },
+    methods: {
+        getData() {
+            this.imgUrl = 'https://storage.googleapis.com/ygoprodeck.com/pics/61665245.jpg';
+        }
     }
-  }
 };
 </script>
 
 <style lang="scss" scoped>
-@import '../sass/global/card.scss';
+@import "../sass/global/card.scss";
 </style>
