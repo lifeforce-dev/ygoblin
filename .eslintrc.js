ddc7cee39d076b0d2bf11a1e2f628c5598d7621c
Joshua Sanders <joshua_sandersr@yahoo.com>
2019-02-24 15:42:18 -0800

    eslint enforce single quotes

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,9 +9,9 @@ module.exports = {
     rules: {
         'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
         'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-        'max-len': ['error', { code: 80 }],
+        'max-len': ['error', { code: 100 }],
         'no-mixed-operators': ['error', { allowSamePrecedence: true }],
-
+        "quotes": [2, "single", { "avoidEscape": true }],
         indent: ['error', 4]
     },
     parserOptions: {
